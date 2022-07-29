b43280814052f784f5379572421e19225e13a825
bcklion1 <bcklion1@gmail.com>
2022-07-29 00:00:21 -0400

    Updates

---
+++ precache-manifest.800e42d184bb2a6b45f96d9151f02454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba1a4bade46b3a9b73cc",
+    "url": "/fcc-challenge-1/static/css/main.668765d0.chunk.css"
+  },
+  {
+    "revision": "ba1a4bade46b3a9b73cc",
+    "url": "/fcc-challenge-1/static/js/main.ba1a4bad.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/fcc-challenge-1/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "0a47c75846c9d6ff6d21",
+    "url": "/fcc-challenge-1/static/css/2.9463ce91.chunk.css"
+  },
+  {
+    "revision": "0a47c75846c9d6ff6d21",
+    "url": "/fcc-challenge-1/static/js/2.0a47c758.chunk.js"
+  },
+  {
+    "revision": "78be2f518ec7c2b6c9d7",
+    "url": "/fcc-challenge-1/static/js/runtime~main.78be2f51.js"
+  },
+  {
+    "revision": "63d62868815160d194549fd3b397b915",
+    "url": "/fcc-challenge-1/index.html"
+  }
+];
